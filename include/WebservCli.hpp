20360ba1903025f0b5eab250dada8dfd2a71fcd9
Essam Dawood <edawood@f1r1s7.codam.nl>
2023-06-22 12:08:30 +0200

    created the CLI class and initialized it

---
+++ include/WebservCli.hpp
@@ -0,0 +1,21 @@
+#ifndef WEBSERVCLI_HPP
+# define WEBSERVCLI_HPP
+
+# include <iostream>
+# include <string>
+# include <sys/utsname.h>
+# include <exception>
+
+class WebservCli {
+    public:
+        WebservCli(void);
+        ~WebservCli(void);
+        static bool check_os(void);
+        class WrongOsException : public std::exception
+        {
+        public:
+            virtual const char *what() const throw();
+        };
+};
+
+#endif
