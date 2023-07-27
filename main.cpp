68380b3744ff26606bdf5004c2f5c4ce46369cc1
Liwen Zhang <lizhang@f1r6s13.codam.nl>
2023-07-27 16:41:05 +0200

    substituted the config-parser part in main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include "include/Server.hpp"
 #include "include/DirSettings.hpp"
 #include "include/KqueueLoop.hpp"
+#include "include/Config.hpp"
 
 int  main(int argc, char *argv[]) 
 {
@@ -39,49 +40,51 @@ int  main(int argc, char *argv[])
 	// set server information
 	try
 	{
-		// dammy data for setting servers before connect Config function  - - - - - - - - - - - - - - - - 
-		std::vector<int> ports1, ports2;
-		ports1.push_back(6868);
-		ports1.push_back(8080);
-		ports2.push_back(6868);
-	
+//		// dammy data for setting servers before connect Config function  - - - - - - - - - - - - - - - -
+//		std::vector<int> ports1, ports2;
+//		ports1.push_back(6868);
+//		ports1.push_back(8080);
+//		ports2.push_back(6868);
+//
+//
+//		DirSettings ds_base1("/", 1);
+//		DirSettings ds1("/test/first", 1);
+//		DirSettings ds1_2("/test/second", 0);
+//		ds_base1.setMethod("GET");
+//		ds_base1.setIndex("index.html");
+//		ds_base1.setError(404, "notfound.html");
+//		ds_base1.setBody(1000);
+//
+//		DirSettings ds_base2("/", 1);
+//		DirSettings ds2("/www", 0);
+//		ds_base2.setMethod("GET");
+//		ds_base2.setMethod("POST");
+//		ds_base2.setIndex("index.htm");
+//		ds_base2.setError(405, "no.html");
+//		ds_base2.setBody(200);
+//
+//
+//		std::vector<DirSettings> optDir;
+//		std::vector<DirSettings> optDir2;
+//		optDir.push_back(ds1);
+//		optDir.push_back(ds2);
+//
+//		Server	newserver("localhost", "./www", ports1); 			// dammy (later, we use parsed info)
+//		Server	newserver2("www.localhost", "./test2", ports2);	// dammy (later, we use parsed info)
+//
+//		newserver.setDirSettings(ds_base1, optDir);
+//		newserver2.setDirSettings(ds_base2, optDir2);
+//
+//		newserver.setSocketListen(kq);
+//		newserver2.setSocketListen(kq);
+//		list_Servers.push_back(newserver);
+//		list_Servers.push_back(newserver2);
+//
+//		// dammy data setting end - - - - - - - - - - - - - - - -
 
-		DirSettings ds_base1("/", 1);
-		DirSettings ds1("/test/first", 1);
-		DirSettings ds1_2("/test/second", 0);
-		ds_base1.setMethod("GET");
-		ds_base1.setIndex("index.html");
-		ds_base1.setError(404, "notfound.html");
-		ds_base1.setBody(1000);
-
-		DirSettings ds_base2("/", 1);
-		DirSettings ds2("/www", 0);
-		ds_base2.setMethod("GET");
-		ds_base2.setMethod("POST");
-		ds_base2.setIndex("index.htm");
-		ds_base2.setError(405, "no.html");
-		ds_base2.setBody(200);
-		
-
-		std::vector<DirSettings> optDir;
-		std::vector<DirSettings> optDir2;
-		optDir.push_back(ds1);
-		optDir.push_back(ds2);
-
-		Server	newserver("localhost", "./www", ports1); 			// dammy (later, we use parsed info)
-		Server	newserver2("www.localhost", "./test2", ports2);	// dammy (later, we use parsed info)
-
-		newserver.setDirSettings(ds_base1, optDir);
-		newserver2.setDirSettings(ds_base2, optDir2);
-
-		newserver.setSocketListen(kq);
-		newserver2.setSocketListen(kq);
-		list_Servers.push_back(newserver);
-		list_Servers.push_back(newserver2);
-
-		// dammy data setting end - - - - - - - - - - - - - - - -
-
-
+        std::string file_path(argv[1]);
+        Config      config(file_path);
+        list_Servers = config.getServers();
 		KqueueLoop	mainloop(&list_Servers, kq);
 		mainloop.startLoop();
 	}
