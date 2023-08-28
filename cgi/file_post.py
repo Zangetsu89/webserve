5ac5ade728f4d1a9b05836906aaea528aeebab7d
Liwen Zhang <lizhang@f1r4s18.codam.nl>
2023-08-28 11:59:13 +0200

    file_post

--- cgi/file_post.py
+++ cgi/file_post.py
@@ -3,6 +3,6 @@ import sys
 success = sys.argv[1:][0]
 print("Content-Type: text/plain")
 if success == "true":
-    print("Data saved successfully.")
+	print("Data saved successfully.")
 else
-    print("Data cannot be saved.")
+	print("Data cannot be saved.")
